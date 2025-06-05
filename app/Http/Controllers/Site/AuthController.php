83ed26e444b540ae36735f0caeba226affbfc59e
Mostafa Saadoun <54408018+mostfa-sadoon@users.noreply.github.com>
2025-06-05 16:52:42 +0300

    doctor register

--- app/Http/Controllers/Site/AuthController.php
+++ app/Http/Controllers/Site/AuthController.php
@@ -6,19 +6,23 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Specialtie;
 Use Alert;
+use App\Enums\AcademicDegrees;
 
 class AuthController extends Controller
 {
     //
     public function index(){
-       return view('Site.Auth.register');
+        $specialties = Specialtie::all();
+        $degrees = AcademicDegrees::options();
+        return view('Site.Auth.register', compact('specialties','degrees'));
     }
 
 
     public function register(Request $request){
 
-     
+        dd($request->all());
 
         $validated = $request->validate([
          'name' => 'required|string|max:255',
--- resources/views/Site/Auth/register.blade.php
+++ resources/views/Site/Auth/register.blade.php
@@ -8,9 +8,9 @@
 
 			<div class="container">
 
-				<span class="pre-title site-color">ابدأ الآن في إنشاء حسابك لتكون دائمًا قريبًا من رعاية صحية عالية الجودة، ولتتمكن من إدارة مواعيدك الصحية بسهولة تامة.</span>
-				<h2 class="main-title">قم بإنشاء حساب كمريض للوصول السريع إلى أفضل خدمات الرعاية الصحية والتشخيصية التي تناسب احتياجاتك وتضمن لك أفضل رعاية طبية.</h2>
-				<p class="main-para"> يسعدنا أن نرحب بك في منصتنا الطبية! قم بإنشاء حساب كمريض لتتمكن من الوصول إلى أفضل الخدمات الصحية المتكاملة. بمجرد إنشاء حسابك، يمكنك حجز المواعيد، متابعة استشاراتك الطبية، والاستفادة من العروض والخدمات الصحية المتميزة. نحن هنا لتوفير الرعاية الصحية بأعلى المعايير لك ولعائلتك.</p>
+				<span class="pre-title site-color">{{__('Start now by creating your account to always stay close to high-quality healthcare and to easily manage your medical appointments')}}</span>
+                <h2 class="main-title">{{__('Create an account as a patient for quick access to the best healthcare and diagnostic services that suit your needs and ensure you receive the highest quality medical care.')}}</h2>
+				<p class="main-para">{{__('We are pleased to welcome you to our medical platform! Create an account as a patient to access the best integrated healthcare services. Once your account is created, you can book appointments, follow up on your medical consultations, and take advantage of exclusive offers and premium healthcare services. We are here to provide healthcare at the highest standards for you and your family.')}}</p>
 
 				<section class="login pdt">
 
@@ -34,54 +34,12 @@
 						<div class="log-doctor">
 							<form class="custom-form text-right" action="{{route('Site.register')}}" method="post">
 								@csrf
-							   <input type="hidden" name="type" value="patient">
+							   <input type="hidden" name="type" value="doctor">
 
-								<div class="form-control">
+
+							   	<div class="form-control">
 									<div class="form-field">
-										<label>اكتب بريدك الإلكتروني</label>
-										<div class="field">
-											<input type="email" name="email" placeholder="example@email.com">
-										     @if ($errors->has('email'))
-                                               <p class="text-danger">{{ $errors->first('email')}}</p>
-                                             @endif
-										</div>
-									</div>			
-								</div>
-								<div class="form-control">
-									<div class="form-field">
-										<label>كلمة المرور</label>
-										<div class="field">
-											<input type="password" name="password" placeholder="">
-											<img class="eye" src="{{asset('siteassets/images/login/eye.svg')}}">
-										</div>
-									</div>			
-								</div>
-
-								<div class="form-control">
-									<div class="form-field flex-between align-center">
-										<span>
-											<input type="checkbox" name=""> تذكرني
-										</span>
-										<a href="#">
-											نسيت كلمة السر؟
-										</a>
-									</div>
-								</div>
-								<div class="form-control">
-									<input type="submit" class="btn w-100"  value="dddd الدخول">
-								</div>
-								<a href="#" class="have-not-account">ليس لديك حساب؟ أنشيء حساب</a>
-							</form>
-						</div>
-						<div class="log-patient active">
-							<form class="custom-form text-right"  action="{{route('Site.register')}}" method="post">
-								@csrf
-			 			   <input type="hidden" name="type" value="patient">
-
-
-								<div class="form-control">
-									<div class="form-field">
-										<label>الاسم</label>
+										<label>{{__('name')}}</label>
 										<div class="field">
 											<input type="text" name="name" value="{{old('name')}}" placeholder="">
 										</div>
@@ -93,7 +51,138 @@
 
 								<div class="form-control">
 									<div class="form-field">
-										<label>اكتب بريدك الإلكتروني</label>
+										<label>{{__('email')}}</label>
+										<div class="field">
+											<input type="email" name="email" placeholder="example@email.com">
+										     @if ($errors->has('email'))
+                                               <p class="text-danger">{{ $errors->first('email')}}</p>
+                                             @endif
+										</div>
+									</div>			
+								</div>
+
+								<div class="form-control">
+									<div class="form-field">
+										<label>{{__('phone')}}</label>
+										<div class="field">
+											<input type="text" name="phone" value="{{old('phone')}}" placeholder="" required>
+											@if ($errors->has('phone'))
+											   <p class="text-danger">{{ $errors->first('phone')}}</p>
+											@endif
+										</div>
+									</div>
+								</div>
+
+
+								<div class="form-control">
+									<div class="form-field">
+										<label>{{__('specialty')}}</label>
+										<div class="field">
+                                           <select class="form-control" required name="specialty">
+											    @foreach ($specialties as $specialty)
+													<option value="{{ $specialty['id'] }}">{{ $specialty['title'] }}</option>
+												@endforeach
+										   </select>	
+										</div>
+									</div>
+								</div>
+
+
+								<div class="form-control">
+									<div class="form-field">
+										<label>{{__('academic degree')}}</label>
+										<div class="field">
+                                           <select class="form-control" required name="academic_degree">
+											    @foreach ($degrees as $degree)
+													<option value="{{ $degree['value'] }}">{{ $degree['label'] }}</option>
+												@endforeach
+										   </select>	
+										</div>
+									</div>
+								</div>
+
+
+								<div class="form-control">
+									<div class="form-field">
+										<label>{{__('graduation year')}}</label>
+										<div class="field">
+                                           <select class="form-control" required name="graduation_year">
+											    @for ($year = 1980; $year <= date('Y'); $year++)
+													<option value="{{ $year }}">{{ $year }}</option>
+												@endfor
+										   </select>	
+										</div>
+									</div>
+								</div>
+
+
+								<div class="form-control">
+									<div class="form-field">
+										<label>{{__('password')}}</label>
+										<div class="field">
+											<input type="password" name="password" placeholder="">
+											<img class="eye" src="{{asset('siteassets/images/login/eye.svg')}}">
+										</div>
+									</div>			
+								</div>
+
+								  <div class="form-control">
+										<div class="form-field">
+											<label>{{__('password confirmation')}}</label>
+											<div class="field">
+												<input type="password" name="password_confirmation" placeholder="">
+												<img class="eye" src="{{asset('siteassets/images/login/eye.svg')}}">
+											</div>
+											@if ($errors->has('password_confirmation'))
+											<p class="text-danger">{{ $errors->first('password_confirmation')}}</p>
+											@endif
+									
+										</div>			
+									</div>
+
+								<div class="form-control">
+									<div class="form-field flex-between align-center">
+										<span>
+											<input type="checkbox" name=""> {{__('remember me')}}
+										</span>
+										<a href="#">
+											{{__('forgot password?')}}
+										</a>
+									</div>
+								</div>
+								{{-- <div class="form-control">
+									<input type="submit" class="btn w-100"  value="{{__('login')}}">
+								</div> --}}
+
+								<div class="form-control">
+									<input type="submit" class="btn w-100" name="register" value="تسجيل الدخول">
+								</div>
+
+
+								<a href="#" class="have-not-account">{{__('don\'t have an account? create one')}}</a>
+							</form>
+						</div>
+						<div class="log-patient active">
+							<form class="custom-form text-right"  action="{{route('Site.register')}}" method="post">
+								@csrf
+			 			   <input type="hidden" name="type" value="patient">
+
+
+								<div class="form-control">
+									<div class="form-field">
+										<label>{{__('name')}}</label>
+										<div class="field">
+											<input type="text" name="name" value="{{old('name')}}" placeholder="">
+										</div>
+										 @if ($errors->has('name'))
+										   <p class="text-danger">{{ $errors->first('name')}}</p>
+										 @endif
+									</div>			
+								</div>			
+
+								<div class="form-control">
+									<div class="form-field">
+										<label>{{__('email')}}</label>
 										<div class="field">
 											<input type="email" name="email" {{old('email')}} placeholder="">
 										</div>
@@ -139,7 +228,7 @@
 
                                 <div class="form-control">
 									<div class="form-field">
-										<label>تأكيد كلمه المرور</label>
+										<label>{{__('password confirmation')}}</label>
 										<div class="field">
 											<input type="password" name="password_confirmation" placeholder="">
 											<img class="eye" src="{{asset('siteassets/images/login/eye.svg')}}">
@@ -167,6 +256,7 @@
 							</form>
 						</div>
 
+				
 					</div>
 				</section>
 			</div>
